452be70356b70c33f859df54bd5400dd6c756cda
MAC <MAC@MACs-MacBook-Pro.local>
2015-06-13 02:11:32 +0545

    Added functionality to grap image path from url

--- app/models/article.rb
+++ app/models/article.rb
@@ -5,4 +5,12 @@ class Article < ActiveRecord::Base
 
   validates :title, presence: true
   validates :url, url: true # Provided by "validate_url" gem
+
+  before_create :fetch_image_url
+
+  private
+  def fetch_image_url
+  	obj = ImageGrabber.new(url)
+  	self.image_url = obj.go_grab
+  end
 end
