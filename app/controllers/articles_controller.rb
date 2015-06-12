4c80548338aedfc3052cfe6e85d2f1d28fbe68c5
gkunwar <MAC@MACs-MacBook-Pro.local>
2015-06-13 03:26:30 +0545

    Removed unnecessary link and arrange article in created date order

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,7 +2,7 @@ class ArticlesController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create]
 
   def index
-    @articles = Article.order(score: :desc)
+    @articles = Article.order(created_at: :desc)
   end
 
   def new
